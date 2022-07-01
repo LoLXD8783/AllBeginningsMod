db21c7d1b24c4a7b7a70a42c413685b7469aba20
Andre, The Sadcat <andreluizmoraeso@hotmail.com>
2022-07-01 18:26:54 -0300

    Agoogus? The Magma Shell
    i'm not sure it works i havent tested
    since i have never used player.lavaCD

--- Content/Items/Accessories/MagmaShellItem.cs
+++ Content/Items/Accessories/MagmaShellItem.cs
@@ -4,24 +4,25 @@ using Terraria.GameContent.Creative;
 
 namespace AllBeginningsMod.Content.Items.Accessories
 {
-    public sealed class PegasusBootsItem : ModItem
+    public sealed class MagmaShellItem : ModItem
     {
         public override void SetStaticDefaults()
         {
-            DisplayName.SetDefault("Pegasus Boots");
-            Tooltip.SetDefault("The wearer can run fast");
+            DisplayName.SetDefault("Magma Shell");
+            Tooltip.SetDefault("Allows for temporary lava immunity" + "\nHowever, the user starts to feel heavier");
             CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
         }
         public override void SetDefaults()
         {
             Item.width = Item.height = 32;
-            Item.value = Item.sellPrice(silver: 80);
+            Item.value = Item.sellPrice(gold: 1);
             Item.accessory = true;
+            Item.defense = 8;
         }
         public override void UpdateEquip(Player player)
         {
-            player.moveSpeed += 0.05f;
-            player.accRunSpeed = 4f;
+            player.moveSpeed -= 0.1f;
+            player.lavaCD = 7;
         }
     }
 }
