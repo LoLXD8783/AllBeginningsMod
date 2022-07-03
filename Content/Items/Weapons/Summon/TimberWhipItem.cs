a5b90fb029f9b47e339e84c418d9a8c007946e7b
Andre, The Sadcat <andreluizmoraeso@hotmail.com>
2022-07-03 04:39:11 -0300

    Added the Timber Whip, a starting whip

---
+++ Content/Items/Weapons/Summon/TimberWhipItem.cs
@@ -0,0 +1,33 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.GameContent.Creative;
+
+namespace AllBeginningsMod.Content.Items.Weapons.Summon
+{
+    public sealed class TimberWhipItem : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 36;
+            Item.height = 46;
+            Item.damage = 8;
+            Item.DamageType = DamageClass.SummonMeleeSpeed;
+            Item.knockBack = 2f;
+            Item.useTime = Item.useAnimation = 30;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.rare = ItemRarityID.Blue;
+            Item.value = Item.sellPrice(silver: 20);
+            Item.DefaultToWhip(ModContent.ProjectileType<Projectiles.Summon.TimberWhipProjectile>(), 8, 2, 3, 25);
+            //Tip: Shoot speed affects the range of the whip
+        }
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.Wood, 12);
+            recipe.AddIngredient(ItemID.Rope, 10);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.Register();
+        }
+    }
+}
---
+++ Content/Projectiles/Summon/TimberWhipProjectile.cs
@@ -0,0 +1,91 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.GameContent;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+
+namespace AllBeginningsMod.Content.Projectiles.Summon
+{
+    public sealed class TimberWhipProjectile : ModProjectile
+    {
+		public override void SetStaticDefaults()
+		{
+			ProjectileID.Sets.IsAWhip[Type] = true;
+		}
+
+		public override void SetDefaults()
+		{
+			Projectile.DefaultToWhip();
+			Projectile.WhipSettings.Segments = 16;
+			Projectile.WhipSettings.RangeMultiplier = 1f;
+		}
+		private float Timer
+		{
+			get => Projectile.ai[0];
+			set => Projectile.ai[0] = value;
+		}
+		public override bool PreDraw(ref Color lightColor)
+		{
+			List<Vector2> list = new List<Vector2>();
+			Projectile.FillWhipControlPoints(Projectile, list);
+
+			SpriteEffects flip = Projectile.spriteDirection < 0 ? SpriteEffects.None : SpriteEffects.FlipHorizontally;
+
+			Main.instance.LoadProjectile(Type);
+			Texture2D texture = TextureAssets.Projectile[Type].Value;
+
+			Vector2 pos = list[0];
+
+			for (int i = 0; i < list.Count - 1; i++)
+			{
+				// These two values are set to suit this projectile's sprite, but won't necessarily work for your own.
+				// You can change them if they don't!
+				Rectangle frame = new Rectangle(0, 0, 14, 32);
+				Vector2 origin = new Vector2(7, 8);
+				float scale = 1;
+
+				// These statements determine what part of the spritesheet to draw for the current segment.
+				// They can also be changed to suit your sprite.
+				if (i == list.Count - 2)
+				{
+					frame.Y = 74;
+					frame.Height = 48;
+
+					// For a more impactful look, this scales the tip of the whip up when fully extended, and down when curled up.
+					Projectile.GetWhipSettings(Projectile, out float timeToFlyOut, out int _, out float _);
+					float t = Timer / timeToFlyOut;
+					scale = MathHelper.Lerp(0.5f, 1.5f, Utils.GetLerpValue(0.1f, 0.7f, t, true) * Utils.GetLerpValue(0.9f, 0.7f, t, true));
+				}
+				else if (i > 10)
+				{
+					frame.Y = 60;
+					frame.Height = 14;
+				}
+				else if (i > 5)
+				{
+					frame.Y = 46;
+					frame.Height = 14;
+				}
+				else if (i > 0)
+				{
+					frame.Y = 32;
+					frame.Height = 14;
+				}
+
+				Vector2 element = list[i];
+				Vector2 diff = list[i + 1] - element;
+
+				float rotation = diff.ToRotation() - MathHelper.PiOver2; // This projectile's sprite faces down, so PiOver2 is used to correct rotation.
+				Color color = Lighting.GetColor(element.ToTileCoordinates());
+
+				Main.EntitySpriteDraw(texture, pos - Main.screenPosition, frame, color, rotation, origin, scale, flip, 0);
+
+				pos += diff;
+			}
+
+			return false;
+		}
+	}
+}
