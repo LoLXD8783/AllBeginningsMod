cc20b1118327499743f5d925056fe8b2be70b0f1
naka <joaovtnakamura@gmail.com>
2022-07-16 22:27:40 -0300

    Icon variations + Upcoming rework

---
+++ Common/Bases/Projectiles/HeldProjectileBase.cs
@@ -0,0 +1,16 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace AllBeginningsMod.Common.Bases.Projectiles
+{
+    public abstract class HeldProjectileBase : ModProjectile
+    {
+        protected Player Owner => Main.player[Projectile.owner];
+
+        public override void AI() {
+            Owner.heldProj = Projectile.whoAmI;
+
+            Projectile.Center = Owner.Center;
+        }
+    }
+}
--- Content/Projectiles/Melee/LeekSwordProjectile.cs
+++ Content/Projectiles/Melee/LeekSwordProjectile.cs
@@ -1,11 +1,10 @@
-﻿using Microsoft.Xna.Framework;
+﻿using AllBeginningsMod.Common.Bases.Projectiles;
 using Terraria;
 using Terraria.ID;
-using Terraria.ModLoader;
 
 namespace AllBeginningsMod.Content.Projectiles.Melee
 {
-    public sealed class LeekSwordProjectile : ModProjectile
+    public sealed class LeekSwordProjectile : HeldProjectileBase
     {
         public override void SetStaticDefaults() {
             DisplayName.SetDefault("Leek Sword");
@@ -19,38 +18,18 @@ namespace AllBeginningsMod.Content.Projectiles.Melee
             Projectile.width = 40;
             Projectile.height = 46;
 
-            Projectile.timeLeft = 20;
+            Projectile.timeLeft = 180;
             Projectile.penetrate = -1;
-
             Projectile.aiStyle = -1;
         }
 
         public override void AI() {
-            Player player = Main.player[Projectile.owner];
 
-            player.armorEffectDrawShadow = true;
-            player.immune = true;
-
-            player.heldProj = Projectile.whoAmI;
-            player.velocity = player.DirectionTo(Main.MouseWorld) * Projectile.timeLeft * 2f;
-
-            player.immuneTime = Projectile.timeLeft;
-
-            if (Projectile.timeLeft == 15) {
-                Projectile.rotation = (Main.MouseWorld - player.Center).ToRotation() + MathHelper.PiOver4 + MathHelper.Pi;
-            }
-
-            Projectile.Center = player.Center;
         }
 
         public override void OnHitNPC(NPC target, int damage, float knockback, bool crit) {
             target.AddBuff(BuffID.DryadsWardDebuff, 120);
             target.AddBuff(BuffID.BrokenArmor, 360);
-        }
-
-        public override void Kill(int timeLeft) {
-            Player player = Main.LocalPlayer;
-            player.velocity = Vector2.Zero;
         }
     }
 }
