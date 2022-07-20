72a67137333026b4d9765c277d4986f2213c6563
Andre, The Sadcat <andreluizmoraeso@hotmail.com>
2022-07-20 00:44:52 -0300

    Added an On NPC Hit effect for the Plume Whip

--- Content/Projectiles/Summon/PlumeWhipProjectile.cs
+++ Content/Projectiles/Summon/PlumeWhipProjectile.cs
@@ -1,5 +1,7 @@
 ﻿using AllBeginningsMod.Common.Bases.Projectiles;
 using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
 
 namespace AllBeginningsMod.Content.Projectiles.Summon
 {
@@ -25,6 +27,13 @@ namespace AllBeginningsMod.Content.Projectiles.Summon
 
             Projectile.WhipSettings.Segments = 14;
             Projectile.WhipSettings.RangeMultiplier = 1f;
+        }
+
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit) {
+            Player player = Main.player[Projectile.owner];
+            for (int i = 0; i < 2; i++) {
+                Projectile.NewProjectile(Projectile.GetSource_FromAI(), target.Center, new Vector2(Main.rand.Next(3, 5) * player.direction, Main.rand.Next(-4, 4)), ModContent.ProjectileType<PlumeWhipFeather>(), damage/2, 0);
+            }
         }
     }
 }
