a1e5b1276a7bcbdb7d195bd6175a59fd93ed0794
naka <joaovtnakamura@gmail.com>
2022-07-29 10:24:45 -0300

    Fixes asset paths in Mod.cs

--- AllBeginningsMod.cs
+++ AllBeginningsMod.cs
@@ -7,8 +7,8 @@ public sealed class AllBeginningsMod : Mod
     public const string AssetsPath = $"{nameof(AllBeginningsMod)}/Assets/";
 
     public const string SoundsPath = AssetsPath + "Sounds/";
+    public const string ExtrasPath = AssetsPath + "Extras/";
     public const string EffectsPath = AssetsPath + "Effects/";
-    public const string TexturesPath = AssetsPath + "Textures/";
 
     public static AllBeginningsMod Instance => ModContent.GetInstance<AllBeginningsMod>();
 }
--- Core/Drawing/Primitives/PrimitiveDrawing.cs
+++ Core/Drawing/Primitives/PrimitiveDrawing.cs
@@ -18,15 +18,13 @@ public sealed class PrimitiveDrawing : ILoadable
 
     void ILoadable.Load(Mod mod) { }
 
-    void ILoadable.Unload() {
-        ThreadUtils.RunOnMainThread(() => {
-            IndexBuffer?.Dispose();
-            IndexBuffer = null;
+    void ILoadable.Unload() => ThreadUtils.RunOnMainThread(() => {
+        IndexBuffer?.Dispose();
+        IndexBuffer = null;
 
-            VertexBuffer?.Dispose();
-            VertexBuffer = null;
-        });
-    }
+        VertexBuffer?.Dispose();
+        VertexBuffer = null;
+    });
 
     public static void DrawPrimitive(VertexPositionColorTexture[] vertices, short[] indices, Effect effect, PrimitiveType type) {
         if (vertices.Length <= 0 || indices.Length <= 0 || effect == null)
@@ -41,13 +39,13 @@ public sealed class PrimitiveDrawing : ILoadable
             IndexBuffer?.Dispose();
             IndexBuffer = new DynamicIndexBuffer(Device, IndexElementSize.SixteenBits, indices.Length, BufferUsage.WriteOnly);
         }
-        
+
         VertexBuffer.SetData(vertices, SetDataOptions.Discard);
         IndexBuffer.SetData(indices, 0, indices.Length, SetDataOptions.Discard);
-        
+
         Device.SetVertexBuffer(VertexBuffer);
         Device.Indices = IndexBuffer;
-        
+
         Device.RasterizerState = RasterizerState.CullNone;
 
         foreach (EffectPass pass in effect.CurrentTechnique.Passes) {
