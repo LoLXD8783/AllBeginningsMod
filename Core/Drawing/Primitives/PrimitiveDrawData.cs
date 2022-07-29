1f62b4f0e61cb57727e71e6e0c9dd7eefcd765cc
naka <joaovtnakamura@gmail.com>
2022-07-29 10:25:47 -0300

    Removes unused PrimitiveDrawData

--- Core/Drawing/Primitives/PrimitiveDrawData.cs
+++
@@ -1,5 +0,0 @@
-﻿using Microsoft.Xna.Framework.Graphics;
-
-namespace AllBeginningsMod.Core.Drawing.Primitives;
-
-public record struct PrimitiveDrawData(VertexPositionColorTexture[] Vertices, ushort[] Indices, Effect Effect, PrimitiveType Type);
