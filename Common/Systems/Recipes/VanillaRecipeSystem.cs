353372ad3de0437bafc4566f374a47fdd034ebe1
naka <joaovtnakamura@gmail.com>
2022-07-30 15:22:41 -0300

    Proper hermes boots recipe

--- Common/Systems/Recipes/VanillaRecipeSystem.cs
+++ Common/Systems/Recipes/VanillaRecipeSystem.cs
@@ -1,4 +1,5 @@
-﻿using Terraria;
+﻿using AllBeginningsMod.Content.Items.Accessories;
+using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -8,6 +9,7 @@ public sealed class VanillaRecipeSystem : ModSystem
 {
     public override void AddRecipes() {
         Recipe recipe = Recipe.Create(ItemID.HermesBoots);
+        recipe.AddIngredient<PegasusBootsItem>();
         recipe.AddIngredient(ItemID.Silk, 15);
         recipe.AddIngredient(ItemID.Feather, 12);
         recipe.AddRecipeGroup(RecipeGroupSystem.PlatinumBar, 5);
