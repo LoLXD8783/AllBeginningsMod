00d09a24e19853bdb87f52fce0e31d055cdd5a20
naka <joaovtnakamura@gmail.com>
2022-08-06 18:46:37 -0300

    Custom window titles

---
+++ Common/CustomWindowTitles.cs
@@ -0,0 +1,20 @@
+﻿using Terraria;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace AllBeginningsMod.Common;
+
+[Autoload(Side = ModSide.Client)]
+public sealed class CustomWindowTitles : ILoadable
+{
+    public const int MaxTitles = 9;
+    
+    void ILoadable.Load(Mod mod) {
+        string selectedTitle = Language.GetTextValue($"Mods.{AllBeginningsMod.ModName}.CustomWindowTitles.Title{Main.rand.Next(MaxTitles)}");
+        Main.instance.Window.Title = $"All Beginnings: {selectedTitle}";
+    }
+
+    void ILoadable.Unload() {
+        Main.changeTheTitle = true;
+    }
+}
