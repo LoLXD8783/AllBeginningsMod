5615db41ac5d8bced4e4c00d10d57e6fcd0c23af
naka <joaovtnakamura@gmail.com>
2022-08-07 16:49:11 -0300

    Several greatsword tweaks

--- Common/Bases/Projectiles/GreatswordProjectileBase.cs
+++ Common/Bases/Projectiles/GreatswordProjectileBase.cs
@@ -13,12 +13,30 @@ using Terraria.ModLoader;
 
 namespace AllBeginningsMod.Common.Bases.Projectiles;
 
-public abstract class GreatswordProjectileBase : ModProjectileBase
+public abstract class GreatswordProjectileBase : HeldProjectileBase
 {
-    public override string Texture => base.Texture.Replace("/Projectiles/", "/Items/Weapons/").Replace("GreatswordProjectile", "GreatswordItem");
+    public const float HoldingState = 0f;
+    public const float ChargingUpState = 1f;
+    public const float AttackingState = 2f;
+    public const float CooldownState = 3f;
+    
+    public ref float CurrentState => ref Projectile.ai[0];
+    
+    public ref float Timer => ref Projectile.ai[1];
+    
+    public abstract float ChargeUpBehindHeadAngle { get; }
+    
+    public abstract float SwingArc { get; }
 
-    protected Player Player => Main.player[Projectile.owner];
+    public abstract int HoldingRadius { get; }
 
+    public abstract Vector2 RotationOrigin { get; }
+
+    public abstract int MaxChargeTimer { get; }
+    public abstract int MaxAttackTimer { get; }
+    public abstract int MaxCooldownTimer { get; }
+    public abstract int MaxSmoothTimer { get; }
+    
     private readonly Func<float, float> easeOut = value => MathF.Log10(9f * value + 1f);
 
     private readonly Func<float, float> easeIn = value => MathF.Pow(value, 3); //x^3 or x^6?
@@ -34,99 +52,61 @@ public abstract class GreatswordProjectileBase : ModProjectileBase
     private int oldDirection;
     
     private int associatedItemType = -1;
-
-    // Variables to tweak motion -> abstract because I don't want to give it 'default' values.
-
-    /// <summary>
-    ///     The maximum upper angle (in radians) the arm will bend backwards when holding the projectile above its head.
-    /// </summary>
-    protected abstract float ChargeUpBehindHeadAngle { get; }
-
-    /// <summary>
-    ///     The swing arc in radians. The swing starts wherever <see cref="ChargeUpBehindHeadAngle" /> is defined at.
-    /// </summary>
-    protected abstract float SwingArc { get; }
-
-    /// <summary>
-    ///     How far should the projectile be from the player.
-    /// </summary>
-    protected abstract int HoldingRadius { get; }
-
-    /// <summary>
-    ///     The point which the texture should rotate around.
-    /// </summary>
-    protected abstract Vector2 RotationOrigin { get; }
-
-    /// <summary>
-    ///     How long the charge up animation will last.
-    /// </summary>
-    protected abstract int MaxChargeTimer { get; }
-
-    /// <summary>
-    ///     How long the attack animation will last.
-    /// </summary>
-    protected abstract int MaxAttackTimer { get; }
-
-    /// <summary>
-    ///     How long the cooldown animation will last.
-    /// </summary>
-    protected abstract int MaxCooldownTimer { get; }
-
-    /// <summary>
-    ///     How long the smooth animation will last.
-    /// </summary>
-    protected abstract int MaxSmoothTimer { get; }
-
-    protected State CurrentState {
-        get => (State) Projectile.ai[0];
-        set => Projectile.ai[0] = (float) value;
-    }
+    
+    public override string Texture => base.Texture.Replace("/Projectiles/", "/Items/Weapons/").Replace("GreatswordProjectile", "GreatswordItem");
 
     public override void SetDefaults() {
-        Projectile.friendly = true;
-        Projectile.hostile = false;
         Projectile.tileCollide = false;
+        
         Projectile.penetrate = -1;
         Projectile.aiStyle = -1;
     }
 
-    protected float Timer {
-        get => Projectile.ai[1];
-        set => Projectile.ai[1] = value;
+    public override void ModifyDamageHitbox(ref Rectangle hitbox) {
+        if (CurrentState != AttackingState) {
+            return;
+        }    
     }
 
+    public override void ModifyHitNPC(NPC target, ref int damage, ref float knockback, ref bool crit, ref int hitDirection) {
+        hitDirection = direction;
+    }
+    
     public override bool PreAI() {
         return !TryKillProjectile();
     }
 
     public override void AI() {
-        Player.heldProj = Projectile.whoAmI;
-        Projectile.active = true;
-
+        base.AI();
+        
         // TODO: Change timers to reflect relevant player multipliers such as attack speed, damage..
+        
+        Projectile.friendly = CurrentState == AttackingState;
 
         float rotationFix = MathHelper.PiOver2 * direction;
         float angleFix = direction == -1 ? MathHelper.Pi : 0f;
 
         switch (CurrentState) {
-            case State.Holding:
+            case HoldingState:
                 // Unit vector from player's center to mouse rotated by 90deg (rotationFix) so that (0,-1) is 0deg.
-                unitVectorToMouse = Vector2.Lerp(unitVectorToMouse, Player.MountedCenter.DirectionTo(Main.MouseWorld).RotatedBy(rotationFix).SafeNormalize(Vector2.UnitY), 0.25f);
+                unitVectorToMouse = Vector2.Lerp(unitVectorToMouse, Owner.MountedCenter.DirectionTo(Main.MouseWorld).RotatedBy(rotationFix).SafeNormalize(Vector2.UnitY), 0.25f);
                 shiftedRotation = unitVectorToMouse.ToRotation();
+
                 FixDirection();
+                    
                 break;
 
-            case State.ChargingUp:
+            case ChargingUpState:
                 // Angle between holding position and the target ChargeUpBehindHeadAngle value.
-                if (Timer == 0) 
+                if (Timer == 0f) 
                     transitionAngle = -ChargeUpBehindHeadAngle * direction - MathHelper.WrapAngle(shiftedRotation + rotationFix + angleFix);
 
                 float chargeProgress = easeOut(Timer / MaxChargeTimer);
                 shiftedRotation = unitVectorToMouse.ToRotation() + transitionAngle * chargeProgress;
 
                 if (Timer++ >= MaxChargeTimer) {
-                    CurrentState = State.Attacking;
-                    Timer = 0;
+                    CurrentState = AttackingState;
+                    Timer = 0f;
 
                     // Setting the fixed direction vector to match the rotation of the last frame of the charge up animation.
                     unitVectorToMouse = shiftedRotation.ToRotationVector2();
@@ -134,13 +114,13 @@ public abstract class GreatswordProjectileBase : ModProjectileBase
 
                 break;
 
-            case State.Attacking:
+            case AttackingState:
                 float attackProgress = easeIn(Timer / MaxAttackTimer);
                 shiftedRotation = unitVectorToMouse.ToRotation() + SwingArc * attackProgress * direction;
 
                 if (Timer++ >= MaxAttackTimer) {
-                    CurrentState = State.Cooldown;
-                    Timer = 0;
+                    CurrentState = CooldownState;
+                    Timer = 0f;
 
                     // Setting the fixed direction vector to match the rotation of the last frame of the charge up animation.
                     unitVectorToMouse = shiftedRotation.ToRotationVector2();
@@ -148,16 +128,17 @@ public abstract class GreatswordProjectileBase : ModProjectileBase
 
                 break;
 
-            case State.Cooldown:
+            case CooldownState:
                 float cooldownProgress = cooldownSmoothCurve(Timer / MaxCooldownTimer);
-                shiftedRotation = unitVectorToMouse.ToRotation() + MathHelper.ToRadians(5) * cooldownProgress * direction;
+                shiftedRotation = unitVectorToMouse.ToRotation() + MathHelper.ToRadians(5f) * cooldownProgress * direction;
 
                 if (Timer++ >= MaxCooldownTimer) {
-                    CurrentState = State.Holding;
-                    Timer = 0;
+                    CurrentState = HoldingState;
+                    Timer = 0f;
 
                     FixDirection();
-                    unitVectorToMouse = Player.MountedCenter.DirectionTo(Main.MouseWorld).RotatedBy(rotationFix).SafeNormalize(Vector2.UnitY);
+                    
+                    unitVectorToMouse = Owner.MountedCenter.DirectionTo(Main.MouseWorld).RotatedBy(rotationFix).SafeNormalize(Vector2.UnitY);
                     shiftedRotation = unitVectorToMouse.ToRotation();
                 }
 
@@ -171,42 +152,38 @@ public abstract class GreatswordProjectileBase : ModProjectileBase
         float frontArmRotation = shiftedRotation - MathHelper.PiOver2 * direction + angleFix;
         float backArmRotation = shiftedRotation - MathHelper.PiOver4 * direction + angleFix;
 
-        Player.SetCompositeArmFront(true, Player.CompositeArmStretchAmount.Full, frontArmRotation);
-        Player.SetCompositeArmBack(true, Player.CompositeArmStretchAmount.ThreeQuarters, backArmRotation);
-
-        // Torso position and rotation?
+        Owner.SetCompositeArmFront(true, Player.CompositeArmStretchAmount.Full, frontArmRotation);
+        Owner.SetCompositeArmBack(true, Player.CompositeArmStretchAmount.ThreeQuarters, backArmRotation);
 
         // Projectile position and rotation.
         Vector2 armRotationOrigin = new(-4f * direction, -2f);
-        Projectile.Center = Player.MountedCenter + armRotationOrigin + shiftedRotation.ToRotationVector2() * HoldingRadius;
+        Vector2 offset = armRotationOrigin + shiftedRotation.ToRotationVector2() * HoldingRadius;
+        HoldoutOffset = offset;
 
         // Projectile rotation is rotated by 45deg to compensate for the 45deg tilt the sprite has.
         Projectile.rotation = shiftedRotation - MathHelper.PiOver4 * direction + angleFix;
+        
+        Owner.ChangeDir(direction);
 
         void FixDirection() {
-            direction = (Main.MouseWorld.X >= Player.Center.X).ToDirectionInt();
-
-            Player.ChangeDir(direction);
+            direction = (Main.MouseWorld.X >= Owner.Center.X).ToDirectionInt();
             
+            Owner.ChangeDir(direction);
+
             if (direction != oldDirection) {
                 oldDirection = direction;
 
                 rotationFix = MathHelper.PiOver2 * direction;
                 angleFix = direction == -1 ? MathHelper.Pi : 0f;
 
-                unitVectorToMouse = Player.MountedCenter.DirectionTo(Main.MouseWorld).RotatedBy(rotationFix).SafeNormalize(Vector2.UnitY);
+                unitVectorToMouse = Owner.MountedCenter.DirectionTo(Main.MouseWorld).RotatedBy(rotationFix).SafeNormalize(Vector2.UnitY);
                 shiftedRotation = unitVectorToMouse.ToRotation();
             }
         }
     }
     
     public override bool PreDraw(ref Color lightColor) {
-        // Making sure movement keys don't take precedence over our desired direction for the player's direction.
-        Player.ChangeDir(direction);
-
-        SpriteEffects spriteEffects = SpriteEffects.None;
-        if (direction == -1)
-            spriteEffects = SpriteEffects.FlipHorizontally;
+        SpriteEffects spriteEffects = direction == -1 ? SpriteEffects.FlipHorizontally : SpriteEffects.None;
 
         Texture2D texture = ModContent.Request<Texture2D>(Texture).Value;
         Rectangle frame = new(0, 0, texture.Width, texture.Height);
@@ -232,37 +209,26 @@ public abstract class GreatswordProjectileBase : ModProjectileBase
 
         return false;
     }
-
+    
+    public void SetAssociatedItemType(int itemType) {
+        associatedItemType = itemType;
+    }
+    
     public void TryAttacking() {
-        if (CurrentState == State.Holding)
-            CurrentState = State.ChargingUp;
+        if (CurrentState == HoldingState)
+            CurrentState = ChargingUpState;
     }
 
     private bool TryKillProjectile() {
-        if ((Player.HeldItem.type != associatedItemType && Projectile.active) ||
-            !Player.active ||
-            Player.dead ||
-            Player.noItems ||
-            Player.CCed) {
+        if ((Owner.HeldItem.type != associatedItemType && Projectile.active) ||
+            !Owner.active ||
+            Owner.dead ||
+            Owner.noItems ||
+            Owner.CCed) {
             Projectile.Kill();
             return true;
         }
 
         return false;
-    }
-
-    /// <summary>
-    ///     Item type required by the player to be held to not despawn the projectile
-    /// </summary>
-    public void SetAssociatedItemType(int itemType) {
-        associatedItemType = itemType;
-    }
-
-    protected enum State
-    {
-        Holding,
-        ChargingUp,
-        Attacking,
-        Cooldown
     }
 }
--- Content/Projectiles/Melee/GoldGreatswordProjectile.cs
+++ Content/Projectiles/Melee/GoldGreatswordProjectile.cs
@@ -5,23 +5,23 @@ namespace AllBeginningsMod.Content.Projectiles.Melee;
 
 public class GoldGreatswordProjectile : GreatswordProjectileBase
 {
-    protected override float ChargeUpBehindHeadAngle => MathHelper.Pi / 6f;
-    protected override float SwingArc => 4 * MathHelper.Pi / 3f;
-    protected override int HoldingRadius => 14;
-    protected override Vector2 RotationOrigin => new(10f, 38f);
-    protected override int MaxChargeTimer => 30;
-    protected override int MaxAttackTimer => 15;
-    protected override int MaxCooldownTimer => 15;
-    protected override int MaxSmoothTimer => 15;
+    public override float ChargeUpBehindHeadAngle => MathHelper.Pi / 6f;
+
+    public override float SwingArc => 4f * MathHelper.Pi / 3f;
+    
+    public override int HoldingRadius => 14;
+    
+    public override Vector2 RotationOrigin => new(10f, 38f);
+    
+    public override int MaxChargeTimer => 30;
+    public override int MaxAttackTimer => 15;
+    public override int MaxCooldownTimer => 15;
+    public override int MaxSmoothTimer => 15;
 
     public override void SetDefaults() {
         base.SetDefaults();
 
         Projectile.width = 46;
         Projectile.height = 46;
-    }
-
-    public override void AI() {
-        base.AI();
     }
 }
