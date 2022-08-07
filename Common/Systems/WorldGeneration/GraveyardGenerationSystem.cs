0678bb1f641c33cb2ea1a6f659e6e50b50b1c3d0
naka <joaovtnakamura@gmail.com>
2022-08-07 19:54:50 -0300

    Graveyards

--- Common/Systems/WorldGeneration/GraveyardGenerationSystem.cs
+++ Common/Systems/WorldGeneration/GraveyardGenerationSystem.cs
@@ -2,11 +2,4 @@
 
 namespace AllBeginningsMod.Common.Systems.WorldGeneration;
 
-public sealed class GraveyardGenerationSystem : ModSystem
-{
-    public override void PostWorldGen() { }
-
-    private static bool TryGenerateGraveyard(int i, int j) {
-        return false;
-    }
-}
+public sealed class GraveyardGenerationSystem : ModSystem { }
