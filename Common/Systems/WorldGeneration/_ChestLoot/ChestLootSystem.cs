53aa98f251cadd52899c50aba1501db91d3fe5e1
naka <joaovtnakamura@gmail.com>
2022-10-27 23:03:28 -0300

    Reworks chest loot

--- Common/Systems/WorldGeneration/_ChestLoot/ChestLootSystem.cs
+++ Common/Systems/WorldGeneration/_ChestLoot/ChestLootSystem.cs
@@ -1,4 +1,10 @@
-﻿using Terraria;
+﻿using System;
+using System.Collections.Generic;
+using AllBeginningsMod.Content.Items.Accessories;
+using AllBeginningsMod.Content.Items.Consumables;
+using AllBeginningsMod.Content.Items.Weapons.Summon;
+using AllBeginningsMod.Utilities;
+using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -6,25 +12,66 @@ namespace AllBeginningsMod.Common.Systems.WorldGeneration;
 
 public sealed class ChestLootSystem : ModSystem
 {
+    private static readonly List<ChestLootEntry> registeredLoot = new();
+    private static readonly Dictionary<int, int> itemAmountByType = new();
+
+    public override void SetStaticDefaults() {
+        registeredLoot.Add(new ChestLootEntry(ModContent.ItemType<PlumeWhipItem>(), 1, 1, 1, 12 * 36));
+        registeredLoot.Add(new ChestLootEntry(ModContent.ItemType<PegasusBootsItem>(), 1, 1, 1, 0, 12 * 36));
+        registeredLoot.Add(new ChestLootEntry(ModContent.ItemType<MidasPouchItem>(), 4, 1, 10, 0, 12 * 36));
+    }
+
     public override void PostWorldGen() {
-        for (int i = 0; i < Main.maxChests; i++) {
-            Chest chest = Main.chest[i];
-
-            if (chest == null) {
-                continue;
+        for (int i = 0; i < registeredLoot.Count; i++) {
+            for (int j = 0; j < Main.maxChests; j++) {
+                ApplyLootEntry(Main.chest[j], registeredLoot[i]);
             }
-
-            SetChestLoot(Framing.GetTileSafely(chest.x, chest.y), chest);
         }
     }
 
-    private static void SetChestLoot(Tile tile, Chest chest) {
-        foreach (ChestLoot chestLoot in ModContent.GetContent<ChestLoot>()) {
-            if (!tile.HasTile || tile.TileType != TileID.Containers || tile.TileFrameX != chestLoot.ChestFrame) {
-                continue;
-            }
-
-            chestLoot.SetLoot(chest);
+    private static void ApplyLootEntry(Chest chest, ChestLootEntry entry) {
+        if (chest == null) {
+            return;
         }
+
+        Tile tile = Framing.GetTileSafely(chest.x, chest.y);
+
+        if (!MatchesChestFrame(tile, entry) || !MatchesTileConditions(tile)) {
+            return;
+        }
+
+        bool alreadyExists = itemAmountByType.TryGetValue(entry.Type, out int amount) && amount > 0;
+        bool shouldAdd = WorldGen.genRand.NextBool(entry.Chance);
+
+        if (alreadyExists && !shouldAdd) {
+            return;
+        }
+
+        int stack = entry.MinStack == 1 && entry.MaxStack == 1 ? 1 : WorldGen.genRand.Next(entry.MinStack, entry.MaxStack);
+
+        if (chest.TryAddItem(entry.Type, stack)) {
+            if (alreadyExists) {
+                itemAmountByType[entry.Type] += stack;
+            }
+            else {
+                itemAmountByType[entry.Type] = stack;
+            }
+        }
+        
+        Console.WriteLine($"{chest.x}X{chest.y} @ {entry.Type}/{entry.MinStack}/{entry.MaxStack}");
+    }
+
+    private static bool MatchesChestFrame(Tile tile, ChestLootEntry entry) {
+        for (int i = 0; i < entry.ChestFrames.Length; i++) {
+            if (tile.TileFrameX == entry.ChestFrames[i]) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static bool MatchesTileConditions(Tile tile) {
+        return tile.HasTile && tile.TileType == TileID.Containers;
     }
 }
--- Utilities/_Extensions/ChestExtensions.cs
+++ Utilities/_Extensions/ChestExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Reflection;
+using AllBeginningsMod.Common.Systems.WorldGeneration;
 using Terraria;
 using Terraria.ID;
 using Terraria.UI;
@@ -9,31 +10,8 @@ namespace AllBeginningsMod.Utilities;
 
 public static class ChestExtensions
 {
-    private static readonly Dictionary<int, int> itemAmountByType;
-
-    private static readonly Action<Item[], int[]> sortAction;
-
-    static ChestExtensions() {
-        itemAmountByType = new Dictionary<int, int>();
-
-        MethodInfo sortMethodInfo = typeof(ItemSorting).GetMethod("Sort", BindingFlags.Static | BindingFlags.NonPublic);
-        sortAction = sortMethodInfo.CreateDelegate<Action<Item[], int[]>>();
-    }
-
-    public static bool TrySort(this Chest chest, params int[] ignoreSlots) {
-        if (!chest.HasAnyItem()) {
-            return false;
-        }
-
-        sortAction(chest.item, ignoreSlots);
-
-        return true;
-    }
-
     public static bool TryAddItem(this Chest chest, int type, int stack = 1) {
-        int index = chest.FindEmptySlotIndex();
-
-        if (index == -1 || type == ItemID.None) {
+        if (!chest.TryGetEmptySlot(out int index) || type == ItemID.None) {
             return false;
         }
 
@@ -44,7 +22,7 @@ public static class ChestExtensions
     }
 
     public static bool TryAddShopItem(this Chest chest, int type, ref int nextSlot) {
-        if (nextSlot == -1 || type == ItemID.None) {
+        if (!chest.TryGetEmptySlot(out _) || type == ItemID.None || nextSlot == -1) {
             return false;
         }
 
@@ -54,49 +32,19 @@ public static class ChestExtensions
         return true;
     }
 
-    public static bool TryAddLootItem(this Chest chest, int type, int stack = 1, int chance = 1) {
-        bool alreadyExists = itemAmountByType.TryGetValue(type, out int amount) && amount > 0;
-        bool shouldAdd = WorldGen.genRand.NextBool(chance);
-
-        if (alreadyExists && !shouldAdd) {
-            return false;
-        }
-
-        bool success = chest.TryAddItem(type, stack);
-
-        if (success) {
-            if (alreadyExists) {
-                itemAmountByType[type] += stack;
-            }
-            else {
-                itemAmountByType[type] = stack;
-            }
-        }
-
-        return success;
-    }
-
-    public static bool HasAnyItem(this Chest chest) {
+    public static bool TryGetEmptySlot(this Chest chest, out int index) {
         for (int i = 0; i < Chest.maxItems; i++) {
             Item item = chest.item[i];
+            
+            if (item != null && item.IsAir) {
+                index = i;
 
-            if (!item.IsAir) {
                 return true;
             }
         }
 
+        index = -1;
+
         return false;
-    }
-
-    public static int FindEmptySlotIndex(this Chest chest) {
-        for (int i = 0; i < Chest.maxItems; i++) {
-            Item item = chest.item[i];
-
-            if (item.IsAir) {
-                return i;
-            }
-        }
-
-        return -1;
     }
 }
