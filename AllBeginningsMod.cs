fcc42c33e7933bd2479ebf2665fe2dacccd27592
naka <joaovtnakamura@gmail.com>
2022-11-12 17:59:36 -0300

    prim buffer

--- AllBeginningsMod.cs
+++ AllBeginningsMod.cs
@@ -1,9 +1,12 @@
-﻿using Terraria.ModLoader;
+﻿using AllBeginningsMod.Common.Graphics;
+using Terraria.ModLoader;
 
 namespace AllBeginningsMod;
 
 public sealed class AllBeginningsMod : Mod
 {
+    public static PrimitiveDrawing PrimitiveDrawing => ModContent.GetInstance<PrimitiveDrawing>();
+    
     public const string ModName = nameof(AllBeginningsMod);
     public const string ModPrefix = ModName + ":";
 }
--- Common/Graphics/_Primitives/PrimitiveDrawing.cs
+++ Common/Graphics/_Primitives/PrimitiveDrawing.cs
@@ -1,51 +1,20 @@
 ﻿using AllBeginningsMod.Utilities;
 using Microsoft.Xna.Framework.Graphics;
 using Terraria;
+using Terraria.ModLoader;
 
 namespace AllBeginningsMod.Common.Graphics;
 
-public static class PrimitiveDrawing
+public sealed class PrimitiveDrawing : ModSystem
 {
-    private static GraphicsDevice Device => Main.graphics.GraphicsDevice;
+    private static readonly PrimitiveBuffer primitiveBuffer = new();
 
-    private static DynamicIndexBuffer indexBuffer;
-    private static DynamicVertexBuffer vertexBuffer;
-    
     public static void DrawPrimitiveShape(IPrimitiveShape shape, PrimitiveType type, Effect effect) {
-        EnsureCapacity(shape);
-        
-        vertexBuffer.SetData(shape.Vertices, SetDataOptions.Discard);
-        indexBuffer.SetData(shape.Indices, 0, shape.Indices.Length, SetDataOptions.Discard);
-        
-        PrepareForDrawing();
-        
-        int primitiveCount = vertexBuffer.GetPrimitiveCount(type);
+        int primitiveCount = primitiveBuffer.VertexBuffer.GetPrimitiveCount(type);
 
         foreach (EffectPass pass in effect.CurrentTechnique.Passes) {
             pass.Apply();
-            Device.DrawIndexedPrimitives(type, 0, 0, vertexBuffer.VertexCount, 0, primitiveCount);
-        }
-    }
-
-    private static void PrepareForDrawing() {
-        Device.SetVertexBuffer(vertexBuffer);
-        Device.Indices = indexBuffer;
-        
-        Device.RasterizerState = RasterizerState.CullNone;
-    }
-    
-    private static void EnsureCapacity<T>(T shape) where T : IPrimitiveShape {
-        int vertexLength = shape.Vertices.Length;
-        int indexLength = shape.Indices.Length;
-        
-        if (vertexBuffer == null || vertexBuffer.VertexCount < vertexLength) {
-            vertexBuffer?.Dispose();
-            vertexBuffer = new DynamicVertexBuffer(Device, VertexPositionColorTexture.VertexDeclaration, vertexLength, BufferUsage.WriteOnly);
-        }
-
-        if (indexBuffer == null || indexBuffer.IndexCount < indexLength) {
-            indexBuffer?.Dispose();
-            indexBuffer = new DynamicIndexBuffer(Device, IndexElementSize.SixteenBits, indexLength, BufferUsage.WriteOnly);
+            Main.graphics.GraphicsDevice.DrawIndexedPrimitives(type, 0, 0, primitiveBuffer.VertexBuffer.VertexCount, 0, primitiveCount);
         }
     }
 }
