c16a8c9a53b034577720e7f545d5bd541bc1a329
naka <joaovtnakamura@gmail.com>
2022-11-27 01:56:13 -0300

    New mod management

--- AllBeginningsMod.cs
+++ AllBeginningsMod.cs
@@ -1,12 +1,9 @@
-﻿using AllBeginningsMod.Common.Graphics;
-using Terraria.ModLoader;
+﻿using Terraria.ModLoader;
 
 namespace AllBeginningsMod;
 
 public sealed class AllBeginningsMod : Mod
 {
-    public static PrimitiveDrawing PrimitiveDrawing => ModContent.GetInstance<PrimitiveDrawing>();
-    
     public const string ModName = nameof(AllBeginningsMod);
     public const string ModPrefix = ModName + ":";
 }
