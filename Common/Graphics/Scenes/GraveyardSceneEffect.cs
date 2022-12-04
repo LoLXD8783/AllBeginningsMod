9d222494c4914f39be1f1ffbc08e9f902cf66dbd
naka <joaovtnakamura@gmail.com>
2022-12-03 22:34:28 -0300

    Removes glitchy vignette fading

--- Common/Graphics/Scenes/GraveyardSceneEffect.cs
+++ Common/Graphics/Scenes/GraveyardSceneEffect.cs
@@ -7,15 +7,11 @@ namespace AllBeginningsMod.Common.Graphics.Scenes;
 [Autoload(Side = ModSide.Client)]
 public sealed class GraveyardSceneEffect : ModSceneEffect
 {
-    public static float CurrentOpacity { get; private set; }
-
     public override SceneEffectPriority Priority => SceneEffectPriority.Environment;
 
     public override void SpecialVisuals(Player player, bool isActive) {
-        CurrentOpacity = MathHelper.SmoothStep(CurrentOpacity, isActive ? 1f : 0f, 0.1f);
-
         if (isActive) {
-            ScreenShaderSystem.Vignette.Parameters["uOpacity"].SetValue(CurrentOpacity);
+            ScreenShaderSystem.Vignette.Parameters["uOpacity"].SetValue(1f);
             ScreenShaderSystem.Vignette.Parameters["strength"].SetValue(0.8f);
             ScreenShaderSystem.Vignette.Parameters["curvature"].SetValue(0.5f);
             ScreenShaderSystem.Vignette.Parameters["innerRadius"].SetValue(0.5f);
