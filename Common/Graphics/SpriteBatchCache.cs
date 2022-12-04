f3ea9d35aeabc121d5a2baff4041d81bb331819c
naka <joaovtnakamura@gmail.com>
2022-12-03 22:26:06 -0300

    SpriteBatch Snapshots re-implemented

---
+++ Common/Graphics/SpriteBatchCache.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using AllBeginningsMod.Utilities;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.ModLoader;
+
+namespace AllBeginningsMod.Common.Graphics;
+
+public sealed class SpriteBatchCache : ILoadable
+{
+    public static FieldInfo SortMode { get; private set; }
+    public static FieldInfo BlendState { get; private set; }
+    public static FieldInfo SamplerState { get; private set; }
+    public static FieldInfo DepthStencilState { get; private set; }
+    public static FieldInfo RasterizerState { get; private set; }
+    public static FieldInfo Effect { get; private set; }
+    public static FieldInfo TransformMatrix { get; private set; }
+
+    void ILoadable.Load(Mod mod) {
+        SortMode = typeof(SpriteBatch).GetField("sortMode", ReflectionUtils.FlagsPrivateInstance);
+        BlendState = typeof(SpriteBatch).GetField("blendState", ReflectionUtils.FlagsPrivateInstance);
+        SamplerState = typeof(SpriteBatch).GetField("samplerState", ReflectionUtils.FlagsPrivateInstance);
+        DepthStencilState = typeof(SpriteBatch).GetField("depthStencilState", ReflectionUtils.FlagsPrivateInstance);
+        RasterizerState = typeof(SpriteBatch).GetField("rasterizerState", ReflectionUtils.FlagsPrivateInstance);
+        Effect = typeof(SpriteBatch).GetField("customEffect", ReflectionUtils.FlagsPrivateInstance);
+        TransformMatrix = typeof(SpriteBatch).GetField("transformMatrix", ReflectionUtils.FlagsPrivateInstance);
+    }
+
+    void ILoadable.Unload() {
+        SortMode = null;
+        BlendState = null;
+        SamplerState = null;
+        DepthStencilState = null;
+        RasterizerState = null;
+        Effect = null;
+        TransformMatrix = null;
+    }
+}
---
+++ Utilities/ReflectionUtils.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+
+namespace AllBeginningsMod.Utilities;
+
+public static class ReflectionUtils
+{
+    public const BindingFlags FlagsAll = BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic;
+
+    public const BindingFlags FlagsPrivate = BindingFlags.NonPublic;
+    public const BindingFlags FlagsPublic = BindingFlags.Public;
+
+    public const BindingFlags FlagsPublicInstance = BindingFlags.Public | BindingFlags.Instance;
+    public const BindingFlags FlagsPrivateInstance = BindingFlags.NonPublic | BindingFlags.Instance;
+}
