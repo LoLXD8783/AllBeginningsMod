f4605ccc19d384e5db8800a6cd0f4c560ef6029b
sucs28mee <adamdudkiewicz2003@gmail.com>
2024-01-04 17:51:20 +0100

    BIG Guh

---
+++ Common/Loaders/ImageStructureLoader.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Terraria.ModLoader;
+
+namespace AllBeginningsMod.Common;
+
+public class ImageStructureLoader : ILoadable
+{
+    private static readonly Dictionary<string, Color[]> images = new();
+    public void Load(Mod mod) {
+        
+    }
+
+    public void Unload() {
+        
+    }
+}
